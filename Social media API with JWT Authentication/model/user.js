c3942c60f01fb851b84fe5a07e3cde3432f5c5af
Arnold-git <arnoldighiwiyisi@gmail.com>
2022-01-04 17:17:39 +0100

    user model :zap:

--- Social media API with JWT Authentication/model/user.js
+++ Social media API with JWT Authentication/model/user.js
@@ -23,6 +23,7 @@ const userSchema = new mongoose.Schema({
   },
   passwordConfirm: {
     type: String,
+    required: [true, 'Please, confirm your password'],
     validate: {
       validator: function (passwordConfirm) {
         return passwordConfirm === this.password;
