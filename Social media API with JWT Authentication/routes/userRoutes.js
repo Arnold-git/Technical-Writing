5c5c6cc561900c76f7ddbfb909436ca16a267818
Arnold-git <arnoldighiwiyisi@gmail.com>
2022-01-05 21:12:14 +0100

    :fire:

--- Social media API with JWT Authentication/routes/userRoutes.js
+++ Social media API with JWT Authentication/routes/userRoutes.js
@@ -1,2 +1,8 @@
 const express = require('express');
 const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
+
+const router = express.Router();
+
+router.post('/signup', authController.signup);
+// router.post('/login', )
