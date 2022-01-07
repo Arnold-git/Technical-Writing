93f24cd534cfda9bc101b8c1545af9aaba283d6b
Arnold-git <arnoldighiwiyisi@gmail.com>
2022-01-07 21:49:02 +0100

    :fire:

--- Social media API with JWT Authentication/models/userModel.js
+++ Social media API with JWT Authentication/models/userModel.js
@@ -39,6 +39,11 @@ const userSchema = new mongoose.Schema({
 //**Document Middleware to Encrypt Password */
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
